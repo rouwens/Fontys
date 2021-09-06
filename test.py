434a337929e3ce5dd1715b8e3fb07acd21a2a610
rouwens <57994396+rouwens@users.noreply.github.com>
2021-09-06 08:24:17 +0200

    Bijgewerkt

--- test.py
+++
@@ -1,7 +0,0 @@
-a_file = open("sample.txt", "r")
-list_of_lines = a_file.readlines()
-list_of_lines[29] = "$cfg['Servers'][$i]['host'] = '192.168.123.13';\n"
-
-a_file = open("sample.txt", "w")
-a_file.writelines(list_of_lines)
-a_file.close()
