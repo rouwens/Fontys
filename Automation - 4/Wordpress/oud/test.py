f883fdf8b39af62a807d631a361676bd71cb005f
rouwens <57994396+rouwens@users.noreply.github.com>
2021-09-08 08:38:45 +0200

    Bijgewerkt

--- Automation - 4/Wordpress/oud/test.py
+++ Automation - 4/Wordpress/oud/test.py
@@ -1,5 +1,12 @@
-cleandomain = "rouwenslan"
+import re 
 
-wp_config_usr = "define( 'DB_USER', '" + cleandomain +"');\n"
+domain = "rouwens.com"
+haakje = "'"
+ssh = "ssh 192.168.123.13 "
+cmd = "echo daan0409 |sudo -S mysql -u root -e "
+sql = "'create database "
+database = domain + ";'"
 
-print (wp_config_db)
+print (ssh + haakje + cmd + sql + database + haakje)
+test = re.sub(r'[^\w\s]', '', cmd)
+print (test)
