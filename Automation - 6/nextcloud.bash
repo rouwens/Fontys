0093a3af8a3178078a073ddf9203445e31f9c776
rouwens <57994396+rouwens@users.noreply.github.com>
2021-10-01 22:12:18 +0200

    Update nextcloud.bash

--- Automation - 6/nextcloud.bash
+++ Automation - 6/nextcloud.bash
@@ -13,13 +13,6 @@ wget https://files.phpmyadmin.net/phpMyAdmin/5.1.1/phpMyAdmin-5.1.1-all-language
 unzip phpMyAdmin-5.1.1-all-languages.zip
 mv phpMyAdmin-5.1.1-all-languages phpmyadmin
 mv phpmyadmin /var/www/html/
-apt install mariadb-server open-vm-tools -y
-hostnamectl set-hostname maas-sql
-cd /etc/mysql/mariadb.conf.d/
-rm 50-server.cnf
-wget https://raw.githubusercontent.com/rouwens/Fontys/S3/Automation%20-%206/50-server.cnf
-systemctl restart mysql
-mysql -u root -e "CREATE USER 'daan'@'%' IDENTIFIED BY 'daan0409';"
-mysql -u root -e "GRANT ALL PRIVILEGES ON *.* TO 'daan'@'%' IDENTIFIED BY 'daan0409';"
-mysql -u root -e "CREATE DATABASE nextcloud;"
-mysql -u root -e "GRANT GRANT OPTION ON *.* TO 'daan'@'%';';
+cd /var/www/html/phpmyadmin
+wget https://raw.githubusercontent.com/rouwens/Fontys/S3/Automation%20-%206/config.inc.php
+chmod 0644 config.inc.php
