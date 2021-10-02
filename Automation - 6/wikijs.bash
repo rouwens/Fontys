bd41b2a7521d08914b1ce76e11c223dcb09ca49c
rouwens <57994396+rouwens@users.noreply.github.com>
2021-10-02 08:46:53 +0200

    Update wikijs.bash

--- Automation - 6/wikijs.bash
+++ Automation - 6/wikijs.bash
@@ -7,3 +7,12 @@ wget https://github.com/Requarks/wiki/releases/download/2.5.219/wiki-js.tar.gz
 mkdir wiki
 tar xzf wiki-js.tar.gz -C ./wiki
 cd ./wiki
+wget https://raw.githubusercontent.com/rouwens/Fontys/S3/Automation%20-%206/config.yml
+cd /home/ubuntu
+mv wiki /var/wiki
+rm wiki-js.tar.gz
+cd /etc/systemd/system
+wget https://raw.githubusercontent.com/rouwens/Fontys/S3/Automation%20-%206/wiki.service
+systemctl daemon-reload
+systemctl start wiki
+systemctl enable wiki
