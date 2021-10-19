e00d9b870dd35a10404ab8eb3b8e2c76a50a4388
rouwens <57994396+rouwens@users.noreply.github.com>
2021-10-19 11:17:09 +0200

    Update wikijs.bash

--- Automation - 6/wikijs.bash
+++ Automation - 6/wikijs.bash
@@ -2,4 +2,4 @@
 apt upgrade -y
 apt install open-vm-tools docker.io -y
 hostnamectl set-hostname maas-wikijs
-docker run -d -p 80:3000 --name wiki --restart unless-stopped -e "DB_TYPE=mysql" -e "DB_HOST=192.168.13.3" -e "DB_PORT=3306" -e "DB_USER=daan" -e "DB_PASS=daan0409" -e "DB_NAME=wiki" requarks/wiki:2
+docker run -d -p 80:3000 --name wiki --restart unless-stopped -e "DB_TYPE=mysql" -e "DB_HOST=192.168.123.13" -e "DB_PORT=3306" -e "DB_USER=daan" -e "DB_PASS=daan0409" -e "DB_NAME=wiki" requarks/wiki:2
