ef0d4a37b3293ae0801827d738af8983e4c4c3fc
rouwens <57994396+rouwens@users.noreply.github.com>
2021-11-14 13:11:44 +0100

    Bijgewerkt

---
+++ Support Services - 3/auto_config.py
@@ -0,0 +1,31 @@
+import socket
+import re
+import mysql.connector as mysql
+import os
+
+# Database connectie
+db = mysql.connect(
+    host = "rouwens.ddns.net",
+    user = "fontys",
+    passwd = "E6g2sAnv4FHBB4HB",
+    database = "scaling_s3",
+    )
+cursor = db.cursor()
+
+# IP ophalen van de machine en interpuncties eruit halen.
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+s.connect(("8.8.8.8", 80))
+ip_heel = (s.getsockname()[0])
+s.close()
+ip = re.sub(r'[^\w\s]', '', ip_heel)
+
+# De gewenste hostname ophalen en doorvoeren
+get_sql = """SELECT hostname FROM `test` WHERE `ip` = %s ;"""
+cursor.execute(get_sql, (ip,))
+sql_hostname = cursor.fetchall()
+
+pre_hostname = str(sql_hostname)
+hostname = pre_hostname[3:-4]
+
+cmd = "hostnamectl set-hostname " + hostname
+os.system(cmd)
